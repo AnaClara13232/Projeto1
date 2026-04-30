ad9744e8762a84f79d750ee00ae01fd70652da26
AnaClara13232 <ana.rabelo@estudante.ifgoiano.edu.br>
2026-04-29 21:04:57 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,93 @@
+<?php
+include "conexao.php";
+
+function validarCPF($cpf) {
+    $cpf = preg_replace('/[^0-9]/', '', $cpf);
+
+    if (strlen($cpf) != 11) return false;
+
+    if (preg_match('/(\d)\1{10}/', $cpf)) return false;
+
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+
+        if ($cpf[$c] != $d) return false;
+    }
+
+    return true;
+}
+
+if (isset($_POST['cadastro'])) {
+
+    $entrada = trim($_POST['emailcelularcpf']);
+    $senha = trim($_POST['senha']);
+
+    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/', $senha)) {
+        echo "<p style='color:red;'>Senha fraca</p>";
+    } else {
+
+        $senha_crip = password_hash($senha, PASSWORD_DEFAULT);
+
+        $emailValido = filter_var($entrada, FILTER_VALIDATE_EMAIL);
+        $celularValido = preg_match('/^[0-9]{10,30}$/', $entrada);
+        $cpfValido = validarCPF($entrada);
+
+        if ($emailValido || $celularValido) {
+            $valorFinal = $entrada;
+        } elseif ($cpfValido) {
+            $valorFinal = hash('sha256', $entrada);
+        } else {
+            echo "<p style='color:red;'>Entrada inválida</p>";
+            $valorFinal = null;
+        }
+
+        if ($valorFinal) {
+
+            $sql = "SELECT id FROM user WHERE email_telefone_cpf = :entrada";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':entrada', $valorFinal);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                echo "<p style='color:red;'>Usuário já existe</p>";
+            } else {
+                $sql = "INSERT INTO user (email_telefone_cpf, senha) VALUES (:entrada, :senha)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':entrada', $valorFinal);
+                $stmt->bindParam(':senha', $senha_crip);
+                $stmt->execute();
+
+                echo "<p style='color:green;'>Conta criada!</p>";
+            }
+        }
+    }
+}
+?>
+
+<html>
+<head>
+    <title>Cadastro</title>
+</head>
+<body>
+
+<h2>Cadastro</h2>
+
+<form method="post">
+    <label>Email/Celular/CPF:</label>
+    <input name="emailcelularcpf" required>
+    <br><br>
+
+    <label>Senha:</label>
+    <input type="password" name="senha" required>
+    <br><br>
+
+    <button type="submit" name="cadastro">Cadastrar</button>
+</form>
+
+<p><a href="login.php">Já tem conta? Fazer login</a></p>
+
+</body>
+</html>
