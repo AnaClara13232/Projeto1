9db9b572a4be61ba309425d062f9db55b2bb6115
Cometa <comas.eo.cara@gmail.com>
2026-05-05 11:31:40 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,67 +1,34 @@
 <?php
 include "conexao.php";
 
-function validarCPF($cpf) {
-    $cpf = preg_replace('/[^0-9]/', '', $cpf);
-
-    if (strlen($cpf) != 11) return false;
-
-    if (preg_match('/(\d)\1{10}/', $cpf)) return false;
-
-    for ($t = 9; $t < 11; $t++) {
-        for ($d = 0, $c = 0; $c < $t; $c++) {
-            $d += $cpf[$c] * (($t + 1) - $c);
-        }
-        $d = ((10 * $d) % 11) % 10;
-
-        if ($cpf[$c] != $d) return false;
-    }
-
-    return true;
-}
-
 if (isset($_POST['cadastro'])) {
 
-    $entrada = trim($_POST['emailcelularcpf']);
+    $email = trim($_POST['email']);
     $senha = trim($_POST['senha']);
 
     if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/', $senha)) {
         echo "<p style='color:red;'>Senha fraca</p>";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !str_ends_with("@estudante.ifgoiano.edu.br", $email)) {
+        echo "<p style='color:red;'>Email inválido</p>";
     } else {
 
         $senha_crip = password_hash($senha, PASSWORD_DEFAULT);
 
-        $emailValido = filter_var($entrada, FILTER_VALIDATE_EMAIL);
-        $celularValido = preg_match('/^[0-9]{10,30}$/', $entrada);
-        $cpfValido = validarCPF($entrada);
+        $sql = "SELECT id FROM user WHERE email_telefone_cpf = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
 
-        if ($emailValido || $celularValido) {
-            $valorFinal = $entrada;
-        } elseif ($cpfValido) {
-            $valorFinal = hash('sha256', $entrada);
+        if ($stmt->rowCount() > 0) {
+            echo "<p style='color:red;'>Usuário já existe</p>";
         } else {
-            echo "<p style='color:red;'>Entrada inválida</p>";
-            $valorFinal = null;
-        }
-
-        if ($valorFinal) {
-
-            $sql = "SELECT id FROM user WHERE email_telefone_cpf = :entrada";
+            $sql = "INSERT INTO user (email_telefone_cpf, senha) VALUES (:email, :senha)";
             $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(':entrada', $valorFinal);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':senha', $senha_crip);
             $stmt->execute();
 
-            if ($stmt->rowCount() > 0) {
-                echo "<p style='color:red;'>Usuário já existe</p>";
-            } else {
-                $sql = "INSERT INTO user (email_telefone_cpf, senha) VALUES (:entrada, :senha)";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':entrada', $valorFinal);
-                $stmt->bindParam(':senha', $senha_crip);
-                $stmt->execute();
-
-                echo "<p style='color:green;'>Conta criada!</p>";
-            }
+            echo "<p style='color:green;'>Conta criada!</p>";
         }
     }
 }
@@ -76,8 +43,8 @@ if (isset($_POST['cadastro'])) {
 <h2>Cadastro</h2>
 
 <form method="post">
-    <label>Email/Celular/CPF:</label>
-    <input name="emailcelularcpf" required>
+    <label>Email:</label>
+    <input type="email" name="email" required>
     <br><br>
 
     <label>Senha:</label>
