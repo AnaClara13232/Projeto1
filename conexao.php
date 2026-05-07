f7568b2e7136cc311a4cc79cbbb6206e3426a6dd
AnaClara13232 <ana.rabelo@estudante.ifgoiano.edu.br>
2026-05-07 14:01:02 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $db = "aula4";
 $user = "root";
-$pass = '';
+$pass = "";
 
 $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
