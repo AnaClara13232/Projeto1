20a3e36794f3d37188f51f7ea8f2a18e234f4089
AnaClara13232 <ana.rabelo@estudante.ifgoiano.edu.br>
2026-05-07 16:11:55 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
 include "conexao.php";
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 if (isset($_POST['login'])) {
 
     $email = trim($_POST['email']);
@@ -12,32 +16,30 @@ if (isset($_POST['login'])) {
     $stmt->execute();
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+    var_dump($_SESSION['usuario_id']);
     if ($user && password_verify($senha, $user['senha'])) {
+        $_SESSION['usuario_id'] = $user['id'];
+        $_SESSION['usuario_nome'] = $user['email_telefone_cpf'];
         header("Location: hub.php");
         exit;
     } else {
-        echo "<p style='color:red;'>Login inválido</p>";
+        echo "<p style='color:red;'>Email ou senha incorretos</p>";
     }
 }
 ?>
 
 <html>
-<head>
-    <title>Login</title>
-</head>
+<head><title>Login</title></head>
 <body>
 
 <h2>Login</h2>
 
 <form method="post">
     <label>Email:</label>
-    <input type="email" name="email" required>
-    <br><br>
+    <input type="email" name="email" required><br><br>
 
     <label>Senha:</label>
-    <input type="password" name="senha" required>
-    <br><br>
+    <input type="password" name="senha" required><br><br>
 
     <button type="submit" name="login">Entrar</button>
 </form>
